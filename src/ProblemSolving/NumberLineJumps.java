fcf3e54a1453bbe81483f843b2e2f49bc35c9b9b
majin byanjankar <majin.byanjankar@gmail.com>
2023-06-13 14:13:12 +0545

    Number Line Jumps corrected the condition

--- src/ProblemSolving/NumberLineJumps.java
+++ src/ProblemSolving/NumberLineJumps.java
@@ -3,21 +3,26 @@ package ProblemSolving;
 public class NumberLineJumps {
     public static String kangaroo(int x1, int v1, int x2,int v2){
         String meetPoint = "NO";
-        int kangarooOneDistance = x1 + v1;
-        int kangarooTwoDistance = x2 + v2;
-        while(kangarooOneDistance != kangarooTwoDistance){
-            x1 = kangarooOneDistance;
-            x2 = kangarooTwoDistance;
-            kangarooOneDistance = x1 + v1;
-            kangarooTwoDistance = x2 + v2;
-            if (kangarooOneDistance == kangarooTwoDistance){
-                meetPoint = "YES";
-                break;
-            }
-        }
-        return meetPoint;
+       if(v1 <= v2){
+           return meetPoint;
+       }
+       else{
+           while (x1 < x2){
+               x1 = x1 + v1;
+           }
+           while (x2 > x1){
+               x2 = x2 + v2;
+           }
+           if(x1 == x2){
+               meetPoint = "YES";
+               return meetPoint;
+           }
+            else{
+                return meetPoint;
+           }
+       }
     }
     public static void main(String[] args) {
-        kangaroo(0,3,4,2);
+        kangaroo(21,6,47,3);
     }
 }
