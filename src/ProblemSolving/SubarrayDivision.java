9787072003b85278154eef21c1f744bfcd00eda1
majin byanjankar <majin.byanjankar@gmail.com>
2023-07-25 18:50:23 +0545

    num change

--- src/ProblemSolving/SubarrayDivision.java
+++ src/ProblemSolving/SubarrayDivision.java
@@ -5,15 +5,15 @@ public class SubarrayDivision {
         int count = 0;
         for (int i = 0; i <= s.length-m; i ++){
             int sum = 0;
-               for (int j = i; j < i + m; j ++){
-                   sum = sum + s[j];
-               }
-                if(sum == d){
-                    count++;
-                }
+            for (int j = i; j < i + m; j ++){
+                sum = sum + s[j];
+            }
+            if(sum == d){
+                count++;
+            }
         }
     return count;
-    }
+        }
     public static void main(String[] args) {
         int[] s = {2,2,1,3,2};
         int d = 4;
