54394d8aeb3fea6c65777698316de6e160b90b6a
majin byanjankar <majin.byanjankar@gmail.com>
2023-07-27 14:22:32 +0545

    indention fixed in sub array division file

--- src/ProblemSolving/SubarrayDivision.java
+++ src/ProblemSolving/SubarrayDivision.java
@@ -3,17 +3,17 @@ package ProblemSolving;
 public class SubarrayDivision {
     public static int birthday(int[] s, int d, int m) {
         int count = 0;
-        for (int i = 0; i <= s.length-m; i ++){
+        for (int i = 0; i <= s.length - m; i++) {
             int sum = 0;
-            for (int j = i; j < i + m; j ++){
+            for (int j = i; j < i + m; j++) {
                 sum = sum + s[j];
             }
-            if(sum == d){
+            if (sum == d) {
                 count++;
             }
         }
-    return count;
-        }
+        return count;
+    }
     public static void main(String[] args) {
         int[] s = {2,2,1,3,2};
         int d = 4;
